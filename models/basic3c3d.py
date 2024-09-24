d204df08db90ceba7c36b9bb3a3292b194912c98
Pranav <u7286976@anu.edu.au>
2024-09-24 16:29:19 +1000

    Updated models and configs

--- models/basic3c3d.py
+++ models/basic3c3d.py
@@ -9,21 +9,38 @@ class Basic3C3D(nn.Module):
 
     def __init__(self, num_classes: int = 10):
         super(Basic3C3D, self).__init__()
-        self.layers = nn.Sequential(
-            nn.Conv2d(3, 32, kernel_size=5, padding=2),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
-            nn.Conv2d(32, 32, kernel_size=5, padding=2),
-            nn.ReLU(),
-            nn.AvgPool2d(kernel_size=3, stride=2, padding=1),
-            nn.Conv2d(32, 64, kernel_size=5, padding=2),
-            nn.ReLU(),
-            nn.AvgPool2d(kernel_size=3, stride=2, padding=1),
-            Flatten(),
-            nn.Linear(4 * 4 * 64, 64),
-            nn.ReLU(),
-            nn.Linear(64, num_classes),
-        )
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=5, padding=2)
+        self.relu1 = nn.ReLU()
+        self.pool1 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.conv2 = nn.Conv2d(32, 32, kernel_size=5, padding=2)
+        self.relu2 = nn.ReLU()
+        self.pool2 = nn.AvgPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=5, padding=2)
+        self.relu3 = nn.ReLU()
+        self.pool3 = nn.AvgPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.flatten = Flatten()
+        self.fc1 = nn.Linear(4 * 4 * 64, 64)
+        self.relu4 = nn.ReLU()
+        self.fc2 = nn.Linear(64, num_classes)
 
     def forward(self, x):
-        return self.layers(x)
+        x = self.conv1(x)
+        x = self.relu1(x)
+        x = self.pool1(x)
+
+        x = self.conv2(x)
+        x = self.relu2(x)
+        x = self.pool2(x)
+
+        x = self.conv3(x)
+        x = self.relu3(x)
+        x = self.pool3(x)
+
+        x = self.flatten(x)
+        x = self.fc1(x)
+        x = self.relu4(x)
+        x = self.fc2(x)
+        return x
--- models/convnet.py
+++ models/convnet.py
@@ -9,20 +9,30 @@ class ConvNet(nn.Module):
 
     def __init__(self):
         super(ConvNet, self).__init__()
-        self.layers = nn.Sequential(
-            nn.Conv2d(1, 10, kernel_size=5),
-            nn.MaxPool2d(kernel_size=2),
-            nn.ReLU(),
-            nn.Conv2d(10, 20, kernel_size=5),
-            nn.MaxPool2d(kernel_size=2),
-            nn.ReLU(),
-            nn.Dropout2d(),
-            Flatten(),
-            nn.Linear(320, 50),
-            nn.ReLU(),
-            nn.Dropout(),
-            nn.Linear(50, 10),
-        )
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.pool1 = nn.MaxPool2d(kernel_size=2)
+        self.relu1 = nn.ReLU()
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.pool2 = nn.MaxPool2d(kernel_size=2)
+        self.relu2 = nn.ReLU()
+        self.dropout2d = nn.Dropout2d()
+        self.flatten = Flatten()
+        self.fc1 = nn.Linear(320, 50)
+        self.relu3 = nn.ReLU()
+        self.dropout = nn.Dropout()
+        self.fc2 = nn.Linear(50, 10)
 
     def forward(self, x):
-        return self.layers(x)
+        x = self.conv1(x)
+        x = self.pool1(x)
+        x = self.relu1(x)
+        x = self.conv2(x)
+        x = self.pool2(x)
+        x = self.relu2(x)
+        x = self.dropout2d(x)
+        x = self.flatten(x)
+        x = self.fc1(x)
+        x = self.relu3(x)
+        x = self.dropout(x)
+        x = self.fc2(x)
+        return x
